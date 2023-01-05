f479e8693e388845a5d4e03c2b5a0ea521035d84
VishnuSabu97 <vishnukunnumpurath1@gmail.com>
2023-01-05 18:47:04 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'Shopping App UI',
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
